0ccd1a08a7f617ec09d4fab7b6128a4365097c0c
cuber-dev <118879829+cuber-dev@users.noreply.github.com>
2023-03-26 14:01:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+const navListUl = document.querySelector(".nav-list");
+const navListUlAnchorTags = document.querySelectorAll(".nav-list li a");
+
+const navOpenBtn = document.querySelector("#nav-list-open-btn");
+const navCloseBtn = document.querySelector("#nav-list-close-btn");
+
+function openNavUl(){
+    navListUl.classList.add("show");
+}
+
+function hideNavUl(){
+    navListUl.classList.remove("show");
+}
+
+
+navOpenBtn.addEventListener("click",openNavUl);
+
+navCloseBtn.addEventListener("click",hideNavUl);
+
+window.addEventListener("click",(e) => {
+    const children = e.target;
+    if (!children.matches("ul") && !children.matches("button") && navListUl.classList.contains("show")) {
+        console.log("not matched");
+        hideNavUl();
+    }
+});
+
+
+
+function handleNavUltarget(e){
+    const children = e.target;
+    navListUlAnchorTags.forEach((e) => {
+        if(e === children){
+            e.classList.add("active");
+        }else{
+            e.classList.remove("active");
+        }
+    });
+}
+
+navListUl.addEventListener("click",handleNavUltarget);
